de79ce4013eb7634fc2545bca0c0fb1a00c145c3
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-11-24 20:42:14 +0600

    firebase cofiguration setup completed

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -18,9 +18,9 @@ const Register = () => {
     console.log(data);
   };
 
-  //   useEffect(() => {
-  //     window.scrollTo(0, 0);
-  //   }, []);
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, []);
 
   return (
     <div className="register-bg">
