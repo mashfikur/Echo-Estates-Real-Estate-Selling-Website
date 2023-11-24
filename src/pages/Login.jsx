1b36ca8953c909990398e89110cd1b0bfbc2fca8
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-11-24 19:38:25 +0600

    react hook form functionality added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,15 @@
 import { Helmet } from "react-helmet-async";
 import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
   return (
     <div className="min-h-screen">
       <Helmet>
@@ -24,12 +32,16 @@ const Login = () => {
                 </h1>
                 <p className="py-6">{"Let's"} continue your journey</p>
               </div>
-              <form className="card-body lg:px-40">
+              <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="card-body lg:px-40"
+              >
                 <div className="form-control">
                   <label className="label">
                     <span className="label-text">Email</span>
                   </label>
                   <input
+                    {...register("email")}
                     type="email"
                     placeholder="email"
                     className="input input-bordered focus:outline-none"
@@ -41,6 +53,7 @@ const Login = () => {
                     <span className="label-text">Password</span>
                   </label>
                   <input
+                    {...register("password")}
                     type="password"
                     placeholder="password"
                     className="input input-bordered focus:outline-none"
@@ -56,10 +69,18 @@ const Login = () => {
 
               <div className="divider -mt-4 md:px-40 mb-4">OR</div>
 
-              <div className="flex  mb-4 items-center justify-center ">
+              <div className="flex flex-col  mb-4 items-center justify-center ">
                 <button className="btn shadow-lg border-none rounded-full btn-wide text-main font-semibold">
                   <FcGoogle className="text-xl"></FcGoogle> Sign In With Google
                 </button>
+
+                <p className="my-4 ">
+                  New on this website ? Please{" "}
+                  <Link to="/register" className=" font-semibold underline">
+                    {" "}
+                    Register{" "}
+                  </Link>{" "}
+                </p>
               </div>
             </div>
           </div>
