d082af69119da5e76694d1379e335eb238ae3084
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-11-24 12:47:33 +0600

    footer section created

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -1,15 +1,44 @@
-import { Button } from "@mui/material";
-
-
+import { Button, Container, Typography } from "@mui/material";
+import mainLogo from "../assets/images/main-logo.png";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 const Footer = () => {
-    return (
-        <div>
-            <h1>this is a footer</h1>
-            <Button variant="contained" color="warning" >
-                this is demo
-            </Button>
+  return (
+    <div className="bg-[#d6f2ffb8] py-20 ">
+      <Container
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+        maxWidth="xl"
+      >
+        <div className="flex items-center gap-4">
+          <img style={{ width: "2rem" }} src={mainLogo} alt="" />
+          <Typography
+            sx={{ fontFamily: '"Playfair Display", serif', fontWeight: "600" }}
+            variant="h5"
+          >
+            Echo Estates
+          </Typography>
         </div>
-    );
+        <div className="cursor-pointer my-5 flex items-center gap-5">
+          <FacebookIcon></FacebookIcon>
+          <InstagramIcon></InstagramIcon>
+          <TwitterIcon></TwitterIcon>
+          <LinkedInIcon></LinkedInIcon>
+        </div>
+        <div>
+          <Typography variant="body1">
+            © {new Date().getFullYear()} Echo Estates . All rights reserved{" "}
+          </Typography>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Footer;
