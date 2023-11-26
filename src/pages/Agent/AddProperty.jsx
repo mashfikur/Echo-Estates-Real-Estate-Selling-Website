3225826f6379d965d1dc5a1d7279e97f4df16a81
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-11-26 23:10:17 +0600

    property info stored to database successfully

--- src/pages/Agent/AddProperty.jsx
+++ src/pages/Agent/AddProperty.jsx
@@ -4,19 +4,76 @@ import Box from "@mui/material/Box";
 import Slider from "@mui/material/Slider";
 import useAuth from "../../hooks/useAuth";
 import { useForm } from "react-hook-form";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import toast from "react-hot-toast";
+import { RotatingLines } from "react-loader-spinner";
+
+const apiKey = import.meta.env.VITE_IMAGE_API_KEY;
+const url = `https://api.imgbb.com/1/upload?key=${apiKey}`;
 
 const AddProperty = () => {
   const { user } = useAuth();
+  const axiosPublic = useAxiosPublic();
+  const axiosSecure = useAxiosSecure();
+
+  const [addingData, setAddingData] = React.useState(null);
   const [value, setValue] = React.useState([10, 30]);
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   // handling form
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset } = useForm();
 
   const onSubmit = (data) => {
     console.log({ ...data, price: value });
+    setAddingData(true);
+
+    axiosPublic
+      .post(
+        url,
+        { image: data.image[0] },
+        {
+          headers: {
+            "content-type": "multipart/form-data",
+          },
+        }
+      )
+      .then((res) => {
+        if (res.data.success) {
+          // adding to database
+          const info = {
+            property_title: data.title,
+            property_location: data.location,
+            property_image: res?.data?.data?.display_url,
+            agent_name: data.name,
+            agent_image: user.photoURL,
+            agent_email: data.email,
+            agent_id: user.uid,
+            price_range: value,
+            verfication_status: "pending",
+            isfraud: true,
+          };
+
+          axiosSecure
+            .post("/api/v1/user/add-property", info)
+            .then((res) => {
+              if (res.data.insertedId) {
+                reset();
+                setAddingData(false);
+                setValue([10, 30]);
+                toast.success("Successfully Added Your Property");
+              }
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
@@ -76,6 +133,7 @@ const AddProperty = () => {
                           type="file"
                           className="file-input  file-focus:outline-none input-bordered w-full rounded-lg"
                           {...register("image")}
+                          required
                         />
                       </div>
                     </div>
@@ -127,7 +185,21 @@ const AddProperty = () => {
                   </div>
                   <div className="form-control ">
                     <button className="btn mx-auto rounded-full px-12 shadow-xl border-none  text-white hover:bg-[#323377] bg-[#323377]">
-                      Add Property
+                      {addingData ? (
+                        <>
+                          {" "}
+                          <RotatingLines
+                            strokeColor="grey"
+                            strokeWidth="5"
+                            animationDuration="0.75"
+                            width="30"
+                            visible={true}
+                          />{" "}
+                          {"Adding.."}
+                        </>
+                      ) : (
+                        "Add Property"
+                      )}
                     </button>
                   </div>
                 </form>
