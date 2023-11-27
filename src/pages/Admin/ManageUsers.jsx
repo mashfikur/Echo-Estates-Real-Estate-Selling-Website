2103b7540b1650da8c19d779068eef9a072c6b23
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-11-28 03:37:07 +0600

    user role updating and deleting user method implemented

--- src/pages/Admin/ManageUsers.jsx
+++ src/pages/Admin/ManageUsers.jsx
@@ -3,28 +3,63 @@ import SectionHeading from "../../components/Dashboard/SectionHeading";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
 import { Button } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { useState } from "react";
 import { ThreeCircles } from "react-loader-spinner";
+import Swal from "sweetalert2";
+import toast from "react-hot-toast";
 
 const ManageUsers = () => {
   const axiosSecure = useAxiosSecure();
-  const [users, setUsers] = useState([]);
-  const [agents, setAgents] = useState([]);
 
-  const { data, isPending } = useQuery({
+  const { data, isPending, refetch } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const res = await axiosSecure.get("/api/v1/admin/get-users");
-
-      const usersArr = res.data.filter((user) => user.role === "user");
-      const agentsArr = res.data.filter((user) => user.role === "agent");
-
-      setUsers(usersArr);
-      setAgents(agentsArr);
-
       return res.data;
     },
   });
+
+  const handleUpdateUser = (id, role) => {
+    Swal.fire({
+      title: "Are you sure about this?",
+      text: "This will update the user!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes , Do it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure
+          .patch(`/api/v1/admin/update-user/${id}?role=${role}`)
+          .then((res) => {
+            if (res.data.modifiedCount) {
+              toast.success(`Updated the user as ${role.toUpperCase()}`);
+              refetch();
+            }
+          });
+      }
+    });
+  };
+  const handleDeleteUser = (id) => {
+    Swal.fire({
+      title: "Do you want to delete this user?",
+      text: "It can't be reverted!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes , Delete!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/api/v1/admin/delete-user/${id}`).then((res) => {
+          if (res.data.deletedCount) {
+            toast.success(`Deleted the user`);
+            refetch();
+          }
+        });
+      }
+    });
+  };
 
   return (
     <div>
@@ -94,8 +129,24 @@ const ManageUsers = () => {
                             >
                               Admin
                             </Button>
+                          ) : user.role === "fraud" ? (
+                            <Button
+                              sx={{
+                                borderRadius: "25px",
+                                px: "2rem",
+                                py: ".6rem",
+                              }}
+                              variant="contained"
+                              size="small"
+                              color="error"
+                            >
+                              Fraud
+                            </Button>
                           ) : (
                             <Button
+                              onClick={() =>
+                                handleUpdateUser(user.userId, "admin")
+                              }
                               sx={{ borderRadius: "25px", py: ".6rem" }}
                               variant="contained"
                               size="small"
@@ -107,8 +158,24 @@ const ManageUsers = () => {
                         <td>
                           {user.role === "agent" ? (
                             <></>
+                          ) : user.role === "fraud" ? (
+                            <Button
+                              sx={{
+                                borderRadius: "25px",
+                                px: "2rem",
+                                py: ".6rem",
+                              }}
+                              variant="contained"
+                              size="small"
+                              color="error"
+                            >
+                              Fraud
+                            </Button>
                           ) : (
                             <Button
+                              onClick={() =>
+                                handleUpdateUser(user.userId, "agent")
+                              }
                               sx={{ borderRadius: "25px", py: ".6rem" }}
                               variant="contained"
                               size="small"
@@ -121,6 +188,9 @@ const ManageUsers = () => {
                         <td>
                           {user.role === "agent" && (
                             <Button
+                              onClick={() =>
+                                handleUpdateUser(user.userId, "fraud")
+                              }
                               sx={{ borderRadius: "25px", py: ".6rem" }}
                               variant="contained"
                               size="small"
@@ -132,6 +202,7 @@ const ManageUsers = () => {
                         </td>
                         <td>
                           <Button
+                            onClick={() => handleDeleteUser(user.userId)}
                             sx={{ borderRadius: "25px", py: ".6rem" }}
                             variant="contained"
                             color="error"
